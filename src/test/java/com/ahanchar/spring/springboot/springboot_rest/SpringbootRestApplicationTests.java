92651749697022ba0af5c5cd40b7a917f4664933
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-09-27 22:21:27 +0300

    Add files via upload

---
+++ src/test/java/com/ahanchar/spring/springboot/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahanchar.spring.springboot.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
